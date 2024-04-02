66e9d4412665e1ecfb1bc1ed8bc65b0fb87e12f2
MonishBK <monishbk17@gmail.com>
2024-04-02 10:35:47 +0530

    moved components to public view

--- src/publicView/Home.jsx
+++ src/publicView/Home.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { CssBaseline, Container, Grid, Box } from '@mui/material';
-import MyAppBar from '../Components/MyAppBar';
-import MainBanner from '../Components/MainBanner';
-import Sidebar from '../Components/Sidebar';
-import InfoCards from '../Components/InfoCards';
-import WelcomeText from '../Components/WelcomeText';
-import EOICard from '../Components/EOICard';
-import UsefulInfo from '../Components/UsefulInfo';
-import EnvironmentLegislation from '../Components/EnvironmentLegislation';
-import NoticesAndUpdates from '../Components/NoticesAndUpdates';
-import QuickLinks from '../Components/QuickLinks';
-import ServiceHelp from '../Components/ServiceHelp';
-import Gallery from '../Components/Gallery';
-import Footer from '../Components/Footer';
+import MyAppBar from './Components/MyAppBar';
+import MainBanner from './Components/MainBanner';
+import Sidebar from './Components/Sidebar';
+import InfoCards from './Components/InfoCards';
+import WelcomeText from './Components/WelcomeText';
+import EOICard from './Components/EOICard';
+import UsefulInfo from './Components/UsefulInfo';
+import EnvironmentLegislation from './Components/EnvironmentLegislation';
+import NoticesAndUpdates from './Components/NoticesAndUpdates';
+import QuickLinks from './Components/QuickLinks';
+import ServiceHelp from './Components/ServiceHelp';
+import Gallery from './Components/Gallery';
+import Footer from './Components/Footer';
 
 function Home() {
-  console.log("change ");
   return (
     <>
       <CssBaseline />
